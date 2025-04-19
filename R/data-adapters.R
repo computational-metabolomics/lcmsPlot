84f170ba103c9d4fd39f7e233c21bd5152a4c643
Ossama Edbali <ossedb@gmail.com>
2025-04-19 23:15:23 +0100

    feat: Enable plotting raw mzML data (#1)

--- R/data-adapters.R
+++ R/data-adapters.R
@@ -1,10 +1,32 @@
 #' @export
-get_metadata <- function(obj, sample_id_column) {
+get_metadata <- function(obj, sample_id_column, metadata) {
   UseMethod("get_metadata")
 }
 
 #' @export
-get_metadata.XCMSnExp <- function(obj, sample_id_column) {
+get_metadata.character <- function(obj, sample_id_column, metadata) {
+  if (is.null(metadata)) {
+    data.frame(sample_path = obj) %>%
+      mutate(
+        sample_index = row_number(),
+        sample_id = tools::file_path_sans_ext(basename(obj))
+      )
+  } else {
+    metadata %>%
+      mutate(
+        sample_index = row_number(),
+        sample_id = .data[[sample_id_column]],
+        sample_path = obj
+      )
+  }
+}
+
+#' @export
+get_metadata.XCMSnExp <- function(obj, sample_id_column, metadata) {
+  if (!is.null(metadata)) {
+    phenoData(obj) <- new("NAnnotatedDataFrame", metadata)
+  }
+
   phenoData(obj)@data %>%
     mutate(
       sample_index = row_number(),
